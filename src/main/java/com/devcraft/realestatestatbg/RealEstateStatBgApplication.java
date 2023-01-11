d7d88d3e0542b461cf1b0c44961fd5dc86cb1253
Kaloyan Dimitrov <47056188+Kaloyan-Dimitrov@users.noreply.github.com>
2023-01-11 11:40:22 +0200

    scheduled tasks experiment

--- src/main/java/com/devcraft/realestatestatbg/RealEstateStatBgApplication.java
+++ src/main/java/com/devcraft/realestatestatbg/RealEstateStatBgApplication.java
@@ -2,8 +2,10 @@ package com.devcraft.realestatestatbg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RealEstateStatBgApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/devcraft/realestatestatbg/services/AverageRegionPriceService.java
+++ src/main/java/com/devcraft/realestatestatbg/services/AverageRegionPriceService.java
@@ -3,6 +3,7 @@ package com.devcraft.realestatestatbg.services;
 import com.devcraft.realestatestatbg.domain.AverageRegionPrice;
 import com.devcraft.realestatestatbg.repositories.AverageRegionPricesRepository;
 import com.devcraft.realestatestatbg.repositories.RegionRepository;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
@@ -20,5 +21,10 @@ public class AverageRegionPriceService {
 
     public void saveAll(Set<AverageRegionPrice> averageRegionPriceFlux) {
         averageRegionPricesRepository.saveAll(averageRegionPriceFlux);
+    }
+
+    @Scheduled(cron = "40 11 * * 1 ?")
+    public void print() {
+        System.out.println("Hello");
     }
 }
